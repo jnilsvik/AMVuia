78e9170f738f9f68029ed3dc169af4e1322f6199
MariusUIA <mariusbn@uia.no>
2021-10-11 11:49:23 +0200

    This is the database insertion script

--- databaseskript/InsertTools.sql
+++ databaseskript/InsertTools.sql
@@ -1,56 +1,56 @@
 use AMVDatabase;
 
 INSERT INTO Tools(
-        toolName, priceFirstDay, priceAfterFirstDay, toolTypeName, certificateID, maintenance, location
+        toolName, priceFirstDay, priceAfterFirstDay, toolTypeName, certificateID, underMaintenance
         )
 VALUES
-       (       'Orbital Sander', 0, 20, 'Various Tools', null, 0, norway
+       (       'Orbital Sander', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Belt Sander', 0, 20, 'Various Tools', null, 0, norway
+       (       'Belt Sander', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Hand Plane', 0, 20, 'Various Tools', null, 0, norway
+       (       'Hand Plane', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Miter Saw', 0, 20, 'Various Tools', null, 0, norway
+       (       'Miter Saw', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Grass Trimmer', 0, 20, 'Various Tools', null, 0, norway
+       (       'Grass Trimmer', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Hammer Drill', 0, 20, 'Various Tools', null, 0, norway
+       (       'Hammer Drill', 0, 20, 'Various Tools', null, 0
        ),
-       (       'String Trimmer', 0, 20, 'Various Tools', null, 0, norway
+       (       'String Trimmer', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Air Compressor', 0, 20, 'Various Tools', null, 0, norway
+       (       'Air Compressor', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Car Diagnosis ToolUNFINISH', 0, 50, 'Various Tools', null, 0, norway
+       (       'Car Diagnosis ToolUNFINISH', 0, 50, 'Various Tools', null, 0
        ),
-       (       'Vibrating PlateUNFINISH', 0, 50, 'Various Tools', null, 0, norway
+       (       'Vibrating PlateUNFINISH', 0, 50, 'Various Tools', null, 0
        ),
-       (       'Tile Cutter Ceramic', 0, 20, 'Various Tools', null, 0, norway
+       (       'Tile Cutter Ceramic', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Automatic Screwer', 0, 20, 'Various Tools', null, 0, norway
+       (       'Automatic Screwer', 0, 20, 'Various Tools', null, 0
        ),
-       (       'Motorized Wheelbarrow', 0, 50, 'Various Tools', null, 0, norway
+       (       'Motorized Wheelbarrow', 0, 50, 'Various Tools', null, 0
        ),
-       (       'Nailgun Pressurized Large', 0, 20, 'Nailguns', null, 0, norway
+       (       'Nailgun Pressurized Large', 0, 20, 'Nailguns', null, 0
        ),
-       (       'Nailgun Pressurized Small', 0, 20, 'Nailguns', null, 0, norway
+       (       'Nailgun Pressurized Small', 0, 20, 'Nailguns', null, 0
        ),
-       (       'Nailgun Milwaukee Large', 0, 20, 'Nailguns', null, 0, norway
+       (       'Nailgun Milwaukee Large', 0, 20, 'Nailguns', null, 0
        ),
-       (       'Nailgun Milwaukee Medium', 0, 20, 'Nailguns', null, 0, norway
+       (       'Nailgun Milwaukee Medium', 0, 20, 'Nailguns', null, 0
        ),
-       (       'Nailgun Milwaukee Small', 0, 20, 'Nailguns', null, 0, norway
+       (       'Nailgun Milwaukee Small', 0, 20, 'Nailguns', null, 0
        ),
-       (       'Wood Splitter', 0, 50, 'Woodcutting', null, 0, norway
+       (       'Wood Splitter', 0, 50, 'Woodcutting', null, 0
        ),
-       (       'Tile Cutter Wood', 0, 50, 'Woodcutting', null, 0, norway
+       (       'Tile Cutter Wood', 0, 50, 'Woodcutting', null, 0
        ),
-       (       'Trailer Buggy', 0, 50, 'Car Trailers', null, 0, norway
+       (       'Trailer Buggy', 0, 50, 'Car Trailers', null, 0
        ),
-       (       'Trailer Small', 0, 50, 'Car Trailers', null, 0, norway
+       (       'Trailer Small', 0, 50, 'Car Trailers', null, 0
        ),
-       (       'Trailer Cargo', 0, 50, 'Car Trailers', null, 0, norway
+       (       'Trailer Cargo', 0, 50, 'Car Trailers', null, 0
        ),
-       (       'Aerial Work Platform', 100, 100, 'Large Equipment', 1, 0, norway
+       (       'Aerial Work Platform', 100, 100, 'Large Equipment', 1, 0
        ),
-       (       'Power Supply', 0, 50, 'Large Equipment', null, 0, norway
+       (       'Power Supply', 0, 50, 'Large Equipment', null, 0
        );
