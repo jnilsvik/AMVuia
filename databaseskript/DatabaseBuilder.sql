bad53bb633b819a0b8f8e6d3f4371e9e1bdb7e22
Joachim N <jnilsvik@gmail.com>
2021-10-12 15:12:38 +0200

    added insert tools and insert user to the script

--- databaseskript/DatabaseBuilder.sql
+++ databaseskript/DatabaseBuilder.sql
@@ -3,7 +3,7 @@ use AMVDatabase;
 
 -- by Joachim Nilsvik
 
-create table if not exists Tools
+create or replace table Tools
 (   -- tool details
 	toolID integer
 	    unique
@@ -12,6 +12,7 @@ create table if not exists Tools
 	    primary key,
     toolCategory varchar(255),
     toolName varchar(255),
+    certificateID integer, -- not sure if this should be here
     -- tool pricing
     priceInitial double,
     priceAfter double,
@@ -86,4 +87,47 @@ describe ToolsCertificate;
 describe UserCertificate;
 -- booking
 describe BookingSummary;
-    
+
+
+
+-- now that the database is initialised, this will start loading in the data
+-- First up is the tool data. (totally not just a reformatted version of what marius had done)
+INSERT INTO Tools(
+    toolName, priceInitial, priceAfter, toolCategory, certificateID, underMaintenance
+)
+VALUES
+    (       'Orbital Sander', 0, 20, 'Various Tools', null, 0),
+    (       'Belt Sander', 0, 20, 'Various Tools', null, 0),
+    (       'Hand Plane', 0, 20, 'Various Tools', null, 0),
+    (       'Miter Saw', 0, 20, 'Various Tools', null, 0),
+    (       'Grass Trimmer', 0, 20, 'Various Tools', null, 0),
+    (       'Hammer Drill', 0, 20, 'Various Tools', null, 0),
+    (       'String Trimmer', 0, 20, 'Various Tools', null, 0),
+    (       'Air Compressor', 0, 20, 'Various Tools', null, 0),
+    (       'Car Diagnosis ToolUNFINISH', 0, 50, 'Various Tools', null, 0),
+    (       'Vibrating PlateUNFINISH', 0, 50, 'Various Tools', null, 0),
+    (       'Tile Cutter Ceramic', 0, 20, 'Various Tools', null, 0),
+    (       'Automatic Screwer', 0, 20, 'Various Tools', null, 0),
+    (       'Motorized Wheelbarrow', 0, 50, 'Various Tools', null, 0),
+    (       'Nailgun Pressurized Large', 0, 20, 'Nailguns', null, 0),
+    (       'Nailgun Pressurized Small', 0, 20, 'Nailguns', null, 0),
+    (       'Nailgun Milwaukee Large', 0, 20, 'Nailguns', null, 0),
+    (       'Nailgun Milwaukee Medium', 0, 20, 'Nailguns', null, 0),
+    (       'Nailgun Milwaukee Small', 0, 20, 'Nailguns', null, 0),
+    (       'Wood Splitter', 0, 50, 'Woodcutting', null, 0),
+    (       'Tile Cutter Wood', 0, 50, 'Woodcutting', null, 0),
+    (       'Trailer Buggy', 0, 50, 'Car Trailers', null, 0),
+    (       'Trailer Small', 0, 50, 'Car Trailers', null, 0),
+    (       'Trailer Cargo', 0, 50, 'Car Trailers', null, 0),
+    (       'Aerial Work Platform', 100, 100, 'Large Equipment', 1, 0),
+    (       'Power Supply', 0, 50, 'Large Equipment', null, 0);
+
+-- should probably add something to prevent other things than numbers being inserted for phone nr...
+INSERT into Users(userpassword, email, phonenumber, firstname, lastname, unionmember)
+-- passwords needs to be hashed or smth, but idk how
+values ('Hei', 'joachimn@uia.no', null, 'Joachim', 'Nilsvik','0'),
+       ('Hei', 'dilans@uia.no', null, 'Dilan', 'Shwane','0'),
+       ('Hei', 'mariusbn@uia.no', null, 'Marius Berg', 'Nordbø', '0'),
+       ('hei', 'roelandc@uia.no', null, 'Roeland', 'Camps', '0'),
+       ('hei', 'paulfe@uia.no', null, 'Paul', 'Feichtenschlager', '0'),
+       ('hei', 'johannao@uia.no', null, 'Johanna', 'Ockenfels', '0');
