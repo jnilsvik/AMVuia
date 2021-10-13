5accc408589fa3bdcd251695cbb8466ccd45113d
MonkeyFart <kriger-7000@hotmail.com>
2021-10-13 11:16:29 +0200

    database implementation that we made

---
+++ databaseskript/tablebuilder.sql
@@ -0,0 +1,55 @@
+create database if not exists AMVDatabase;
+use AMVDatabase;
+CREATE TABLE ToolCertificate (
+                                 certificateID int NOT NULL auto_increment,
+                                 certificateName VARCHAR(50) NOT NULL,
+                                 PRIMARY KEY (certificateID)
+);
+
+CREATE TABLE Tool (
+                      toolID int NOT NULL auto_increment,
+                      toolName VARCHAR(50) NOT NULL,
+                      maintenance boolean NOT NULL,
+                      priceFirst int NOT NULL,
+                      priceAfter int NOT NULL,
+                      toolCategory VARCHAR(50) NOT NULL,
+                      certificateID int NOT NULL,
+                      PRIMARY KEY (toolID),
+                      FOREIGN KEY (certificateID) REFERENCES ToolCertificate(certificateID),
+                      CHECK(toolCategory in (
+                                             'Various Tools', 'Nailguns', 'Woodcutting', 'Car Trailers', 'Large Equipment'
+                          ))
+);
+
+CREATE TABLE AMVUser (
+                         userID INT NOT NULL auto_increment,
+                         email VARCHAR(50) NOT NULL,
+                         passwordHash VARCHAR(250) NOT NULL,
+                         firstName VARCHAR(50),
+                         lastName VARCHAR(50) NOT NULL,
+                         phoneNumber VARCHAR(16),
+                         unionMember boolean NOT NULL,
+                         userAdmin boolean NOT NULL,
+                         PRIMARY KEY (userID)
+);
+
+CREATE TABLE UsersCertificate (
+                                  userID int NOT NULL,
+                                  certificateID int NOT NULL,
+                                  accomplishDate date NOT NULL,
+                                  Primary key (userID, certificateID, accomplishDate),
+                                  FOREIGN KEY (userID) REFERENCES Users(userID),
+                                  FOREIGN KEY (certificateID) REFERENCES ToolCertificate(certificateID) on delete cascade
+);
+
+CREATE TABLE Booking (
+                         orderID int NOT NULL auto_increment,
+                         startDate date NOT NULL,
+                         endDate date NOT NULL,
+                         totalPrice int NOT NULL,
+                         userID int NOT NULL,
+                         toolID int NOT NULL,
+                         PRIMARY KEY (orderID),
+                         FOREIGN KEY (userID) REFERENCES Users(userID),
+                         FOREIGN KEY (toolID) REFERENCES Tools(toolID)
+);
