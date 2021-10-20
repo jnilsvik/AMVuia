add4ce95c7d33f7cf8c60f7b682094f36014a856
MonkeyFart <kriger-7000@hotmail.com>
2021-10-20 16:51:34 +0200

    Remove the 5 toolcategory serlvets and made it into one servlet for tool categorys and 1 servlet for a list of all the tools

---
+++ src/main/java/bacit/web/toolPages/ToolCategoryServlet.java
@@ -0,0 +1,62 @@
+package bacit.web.toolPages;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+//Marius wrote this code
+
+@WebServlet(name = "ToolCategoryServlet", value = "/toolcategories")
+public class ToolCategoryServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mariadb://172.17.0.1:3308/AMVDatabase", "root", "12345");
+
+            PreparedStatement ps = con.prepareStatement("SELECT toolCategory FROM Tool GROUP BY toolCategory");
+            ResultSet rs = ps.executeQuery();
+
+            out.print("<form action = 'toollistservlet' method = 'GET'");
+            out.print("<label for = 'category'> Choose a tool:</label>");
+            out.print("<select name = 'category' id = 'category'><br>");
+
+
+            while (rs.next()) {
+
+                String categoryName = rs.getString("toolCategory");
+                out.print("<option value = '" + categoryName + "'> " + categoryName + " </option>");
+
+            }
+
+            out.print("</select>");
+            out.println("<input type = 'submit' value = 'Submit'>");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+
+        } catch (Exception e) {
+            out.println("error");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
---
+++ src/main/java/bacit/web/toolPages/ToolListServlet.java
@@ -0,0 +1,64 @@
+package bacit.web.toolPages;
+
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+//Marius wrote this code
+
+@WebServlet(name = "ToolListServlet", value = "/toollistservlet")
+public class ToolListServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try {
+
+            Class.forName("org.mariadb.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mariadb://172.17.0.1:3308/AMVDatabase", "root", "12345");
+
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM Tool WHERE toolCategory = ?");
+            ps.setString(1, (request.getParameter("category")));
+            ResultSet rs = ps.executeQuery();
+
+            out.print("<form action = 'details' method = 'GET'");
+            out.print("<label for = 'tool'> Choose a tool:</label>");
+            out.print("<select name = 'tool' id = 'tool'><br>");
+
+
+            while (rs.next()) {
+
+                String toolName = rs.getString("toolName");
+                int toolID = rs.getInt("toolID");
+                out.print("<option value = '" + toolID + "'> " + toolName + " </option>");
+
+            }
+
+            out.print("</select>");
+            out.println("<input type = 'submit' value = 'Submit'>");
+            out.println("</form>");
+            out.println("</body>");
+            out.println("</html>");
+
+        } catch (Exception e) {
+            out.println("error");
+        }
+    }
+
+}
+
+
+
+
+
+
+
+
