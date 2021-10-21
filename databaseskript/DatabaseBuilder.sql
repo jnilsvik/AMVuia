9a8abfb210a59d74b7e14d03446930a1542c146a
RoelandC <80832844+RoelandC@users.noreply.github.com>
2021-10-21 09:16:03 +0200

    Update DatabaseBuilder.sql

--- databaseskript/DatabaseBuilder.sql
+++ databaseskript/DatabaseBuilder.sql
@@ -42,7 +42,7 @@ CREATE OR REPLACE TABLE Booking (
     totalPrice int NOT NULL,
     userID int NOT NULL,
     toolID int NOT NULL,
-    available boolean NOT NULL,
+    toolReturnDate date NOT NULL,
     PRIMARY KEY (orderID),
     FOREIGN KEY (userID) REFERENCES AMVUser(userID),
     FOREIGN KEY (toolID) REFERENCES Tool(toolID)
