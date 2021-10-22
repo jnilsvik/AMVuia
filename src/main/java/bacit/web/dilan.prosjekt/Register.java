582ee3f31dc8f1edfc9cebdde4d6e2e447114e54
Joachim <jnilsvik@gmail.com>
2021-10-22 15:44:39 +0200

    updated register form, updated html

--- src/main/java/bacit/web/dilan.prosjekt/Register.java
+++ src/main/java/bacit/web/dilan.prosjekt/Register.java
@@ -19,32 +19,31 @@ public class Register extends HttpServlet {
         response.setContentType("text/html");
 
         PrintWriter out = response.getWriter();
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Register user form</title>");
-        out.println("</head>");
-
-        out.println("<h2>Register User</h2>");
-        out.println("<form action = 'register' method = 'POST'> ");
-        out.println("<label for = 'firstname'>First Name: </label><br>");
-        out.println("<input type = 'text' name = 'firstname' required><br>");
-        out.println("<label for = 'lastname' required>Last Name: </label><br>");
-        out.println("<input type = 'text' name = 'lastname' required><br>");
-        out.println("<label for = 'password'>Password: </label><br>");
-        out.println("<input type = 'password' name = 'password' required><br>");
-        out.println("<label for = 'email'>Email: </label><br>");
-        out.println("<input type = 'email' name = 'email' required><br>");
-        out.println("<label for = 'phone'>Phone: </label><br>");
-        out.println("<input type = 'tel' name = 'phone' required minlength=\"5\" pattern=\"[0-9]{5}\"><br>"); //jn: ive set the min. amount to 5 for now
-        out.println("<label for = 'unionmember'>Union Member: </label><br>");
-        out.println("<input type = 'checkbox' name = 'unionmember' value = 'true'><br>");
-        out.println("<label for = 'userAdmin'>Admin: </label><br>");
-        out.println("<input type = 'checkbox' name = 'userAdmin' value = 'true'><br>");
-        out.println("<input type = 'submit' value = 'Register User'>");
-        out.println("</form>");
-
-        out.println("</body>");
-        out.println("</html>");
+        out.println("<html>" +
+                    "<head>" +
+                    "<title>Register user form</title>" +
+                    "</head>" +
+                    "<body>" +
+                        "<h2>Register User</h2>" +
+                        "<form action = 'register' method = 'POST'> " +
+                            "<label for = 'firstname'>First Name: </label><br>" +
+                            "<input type = 'text' name = 'firstname' required><br>" +
+                            "<label for = 'lastname' required>Last Name: </label><br>" +
+                            "<input type = 'text' name = 'lastname' required><br>" +
+                            "<label for = 'password'>Password: </label><br>" +
+                            "<input type = 'password' name = 'password' required><br>" +
+                            "<label for = 'email'>Email: </label><br>" +
+                            "<input type = 'email' name = 'email' required><br>" +
+                            "<label for = 'phone'>Phone: </label><br>" +
+                            "<input type = 'tel' name = 'phone' required pattern=\"[0-9]{5,9}\"><br>" +
+                            "<label for = 'unionmember'>Union Member: </label><br>" +
+                            "<input type = 'checkbox' name = 'unionmember' value = 'true'><br>" +
+                            "<label for = 'userAdmin'>Admin: </label><br>" +
+                            "<input type = 'checkbox' name = 'userAdmin' value = 'true'><br>" +
+                            "<input type = 'submit' value = 'Register User'>" +
+                        "</form>" +
+                    "</body>" +
+                    "</html>");
     }
 
 
