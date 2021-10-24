c39342e40b3030dca271d3421c505f7c38210005
Joachim <jnilsvik@gmail.com>
2021-10-24 14:16:30 +0200

    added a nav bar element you can call (not super functional yet tho)

--- src/main/java/bacit/web/bew/ToolListServlet.java
+++ src/main/java/bacit/web/bew/ToolListServlet.java
@@ -4,6 +4,7 @@ import bacit.web.bacit_database.DBUtils;
 import bacit.web.bacit_headerFooter.HeaderFooter;
 import bacit.web.bacit_models.ToolModel;
 import bacit.web.bacit_models.UserModel;
+import bacit.web.bew.Navbar;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -30,6 +31,8 @@ public class ToolListServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         PrintWriter out = response.getWriter();
         try {
+            Navbar.sidebar(out);
+
             Connection dbConnecton = DBUtils.getNoErrorConnection(out);
             String toolQ = "select * from Tool order by toolID ";
             //could probably change the "toolID" with some variable to have different ordering
@@ -43,58 +46,6 @@ public class ToolListServlet extends HttpServlet {
                         "  <title>Sorting Tables w/ JavaScript</title>" +
                         "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />" +
                         "  <meta charset=\"utf-8\" />" +
-                        "  <style>" +
-                        "    .table-sortable th {" +
-                        "      cursor: pointer;" +
-                        "    .table-sortable .th-sort-asc::after {" +
-                        "      content: \"\\25b4\";" +
-                        "    }" +
-                        "    .table-sortable .th-sort-desc::after {" +
-                        "      content: \"\\25be\";" +
-                        "    }" +
-                        "    .table-sortable .th-sort-asc::after," +
-                        "    .table-sortable .th-sort-desc::after {" +
-                        "      margin-left: 5px;" +
-                        "    }" +
-                        "    .table-sortable .th-sort-asc," +
-                        "    .table-sortable .th-sort-desc {" +
-                        "      background: rgba(0, 0, 0, 0.1);" +
-                        "    }" +
-                        "  </style>" +
-                        "  <script>" +
-                        "    function sortTableByColumn(table, column, asc = true) {" +
-                        "      const dirModifier = asc ? 1 : -1;" +
-                        "      const tBody = table.tBodies[0];" +
-                        "      const rows = Array.from(tBody.querySelectorAll(\"tr\"));" +
-                        "      // Sort each row" +
-                        "      const sortedRows = rows.sort((a, b) => {" +
-                        "        const aColText = a.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();" +
-                        "        const bColText = b.querySelector(`td:nth-child(${ column + 1 })`).textContent.trim();" +
-                        "        return aColText > bColText ? (1 * dirModifier) : (-1 * dirModifier);" +
-                        "      });" +
-                        "      // Remove all existing TRs from the table" +
-                        "      while (tBody.firstChild) {" +
-                        "        tBody.removeChild(tBody.firstChild);" +
-                        "      }" +
-                        "      // Re-add the newly sorted rows" +
-                        "      tBody.append(...sortedRows);" +
-                        "" +
-                        "      // Remember how the column is currently sorted" +
-                        "      table.querySelectorAll(\"th\").forEach(th => th.classList.remove(\"th-sort-asc\", \"th-sort-desc\"));" +
-                        "      table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle(\"th-sort-asc\", asc);" +
-                        "      table.querySelector(`th:nth-child(${ column + 1})`).classList.toggle(\"th-sort-desc\", !asc);" +
-                        "    }" +
-
-                        "    document.querySelectorAll(\".table-sortable th\").forEach(headerCell => {" +
-                        "      headerCell.addEventListener(\"click\", () => {" +
-                        "        const tableElement = headerCell.parentElement.parentElement.parentElement;" +
-                        "        const headerIndex = Array.prototype.indexOf.call(headerCell.parentElement.children, headerCell);" +
-                        "        const currentIsAscending = headerCell.classList.contains(\"th-sort-asc\");" +
-
-                        "        sortTableByColumn(tableElement, headerIndex, !currentIsAscending);" +
-                        "      });" +
-                        "    });" +
-                        "  </script>" +
                         "</head>" +
                         "<body>" +
                         "    <h3>Tool Stuff</h3>" +
