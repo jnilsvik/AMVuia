8918f85f9ae8a5e8792dc64e61618feb52eccad5
MonkeyFart <kriger-7000@hotmail.com>
2021-10-28 15:32:55 +0200

    Made admin fucntion to put tool in and out of maintenance

---
+++ src/main/java/bacit/web/adminPages/ToolMaintenance.java
@@ -0,0 +1,70 @@
+package bacit.web.adminPages;
+
+import bacit.web.bacit_database.DBUtils;
+import bacit.web.LoginRegister.AdminAccess;
+
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.io.*;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalDate;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+// by Dilan
+@WebServlet(name = "ToolMaintenance", value = "/toolmaintenance")
+public class ToolMaintenance extends HttpServlet {
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        try {
+            HttpSession session = request.getSession(false);
+            String email = (String) session.getAttribute("email");
+
+            if (AdminAccess.accessRights(email)) {
+                out.println("<html>");
+                out.println("<head>");
+                out.println("<title>Tool maintenance</title>");
+                out.println("</head>");
+
+                out.println("<h2>Put a tool in maintenance</h2>");
+                out.println("<form action = 'toolmaintenancein' method = 'POST'> ");
+                out.println("<label for = 'toolID'>Tool ID: </label><br>");
+                out.println("<input type = 'text' name = 'toolID'><br>");
+                out.println("<input type = 'submit' value = 'Submit'>");
+                out.println("</form>");
+                out.println("<br>");
+
+                out.println("<h2>Put a tool out of maintenance</h2>");
+                out.println("<form action = 'toolmaintenanceout' method = 'POST'> ");
+                out.println("<label for = 'toolID'>Tool ID: </label><br>");
+                out.println("<input type = 'text' name = 'toolID'><br>");
+                out.println("<input type = 'submit' value = 'Submit'>");
+                out.println("</form>");
+
+                out.println("</body>");
+                out.println("</html>");
+            } else {
+                out.println("<h1> Sorry, you don't have access to this page");
+            }
+
+        } catch (Exception e) {
+            out.println("error");
+        }
+    }
+
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+}
+
+
+
+
