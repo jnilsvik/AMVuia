c6df648aa6e9f63d24c103d3639b4cfc9815252b
MonkeyFart <kriger-7000@hotmail.com>
2021-11-08 16:39:55 +0100

    Reverted back changes

--- src/main/java/bacit/web/adminPages/AdminAccess.java
+++ src/main/java/bacit/web/adminPages/AdminAccess.java
@@ -3,16 +3,8 @@ import java.sql.*;
 
 // by Dilan
 public class  AdminAccess {
-
-    private static boolean adminAlreadySet = false;
-    private static boolean isAdmin = false;
-
-    public static boolean accessRights(String email){
-        if(!adminAlreadySet) setRights(email);
-        return isAdmin;
-    }
-
-    private static void setRights(String email)   {
+    public static boolean accessRights(String email)   {
+        boolean isAdmin = false;
         try {
             Class.forName("org.mariadb.jdbc.Driver");
             Connection con = DriverManager.getConnection("jdbc:mariadb://172.17.0.1:3308/AMVDatabase","root","12345");
@@ -23,14 +15,13 @@ public class  AdminAccess {
             while (rs.next())
             {
                 isAdmin  = rs.getBoolean("userAdmin");
-                adminAlreadySet = true;
             }
 
         }
         catch(Exception e) {
             e.printStackTrace();
         }
+        return isAdmin;
     }
 
 }
-
